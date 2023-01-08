fc689c43a567edf1130b7b90902881fcbba506a8
Matthew Creek <matthew.creek27@gmail.com>
2023-01-07 20:27:25 -0600

    formatting

--- src/exercises/chapter3/ArrayListPractice.java
+++ src/exercises/chapter3/ArrayListPractice.java
@@ -16,15 +16,9 @@ public class ArrayListPractice {
         someIntegers.add(9);
         someIntegers.add(10);
         System.out.println(addEvens(someIntegers));
-        System.out.println(someIntegers.get(9).toString().getClass());
         String excerpt = "I would not, could not, in a box. I would not, could not with a fox. I will not eat them in a house. I will not eat them with a mouse.";
         String[] strSplit = excerpt.split(" ");
         ArrayList<String> someWords = new ArrayList<>(java.util.Arrays.asList(strSplit));
-//        someWords.add("bull");
-//        someWords.add("shit");
-//        someWords.add("talks");
-//        someWords.add("money");
-//        someWords.add("walks");
         print5LetterWords(someWords);
     }
     public static int addEvens(ArrayList<Integer> arr) {
