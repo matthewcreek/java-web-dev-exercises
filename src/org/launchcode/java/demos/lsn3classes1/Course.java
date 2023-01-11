6c6d6193ce34d0f224ae76fdeea95b609ba541b2
Matthew Creek <matthew.creek27@gmail.com>
2023-01-10 21:19:43 -0600

    chapter 4 exercises coded

---
+++ src/org/launchcode/java/demos/lsn3classes1/Course.java
@@ -0,0 +1,12 @@
+package org.launchcode.java.demos.lsn3classes1;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Course {
+    private String topic;
+    private Teacher instructor;
+    private HashMap<Integer, Student> enrolledStudents;
+
+//    private ArrayList<Student> enrolledStudents;
+}
